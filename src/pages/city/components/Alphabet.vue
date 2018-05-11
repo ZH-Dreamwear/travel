34bb4f6a255d30c57348d3bee9122a8a0b8b2f5f
ZH-Dreamwear <1217243450@qq.com>
2018-05-11 19:07:07 +0800

    实现手指触摸滑动右侧字母表时滚动到对应的城市列表

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,25 +1,76 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item, key) of cities" :key="key"
+    <li
+      class="item"
+      v-for="item of letters"
+      :key="item"
+      :ref="item"
+      @touchstart.prevent="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
       @click="handleLetterClick"
     >
-      {{key}}
+      {{item}}
     </li>
   </ul>
 </template>
+
 <script>
 export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
   },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  updated () {
+    //获取字母A到顶端的距离
+    this.startY = this.$refs['A'][0].offsetTop
+  },
   methods: {
     handleLetterClick (e) {
-      this.$emit('change',e.target.innerText)
+      this.$emit('change', e.target.innerText)
     },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        //进行性能优化，减少handleTouchMove的频率
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          //计算手指触摸到的位置至字母A的距离，再除以每个字母的高度，得出要滚动到哪个字母块
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
+
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
   .list
